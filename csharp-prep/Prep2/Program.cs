fc20852736c568818adb6db132af53bdbf18ef68
jgcnsny <thegreathappyvirus@gmail.com>
2024-04-24 22:41:40 +0800

    c# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,75 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your Grade Percentage? ");
+        string grade = Console.ReadLine();
+        int gradePercentage = int.Parse(grade);
+
+        string letterGrade = "";
+
+        if (gradePercentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        //Stretch Challenge 1
+            char sign;
+
+            int last_digit = gradePercentage % 10;
+
+            if (last_digit >= 7)
+            {
+                sign = '+' ;
+            }
+                
+            else if (last_digit < 3)
+            {
+                sign = '-' ;
+            }
+            else
+            {
+                sign = ' ' ;
+            }
+
+        //Stretch Challenge 2
+            if (gradePercentage >= 93)
+            {
+                sign = ' ';
+            }
+
+        //Stretch Challenge 3
+            if (letterGrade == "F")
+            {
+                sign = ' ';
+            }
+                
+
+        Console.WriteLine($"Your Grade is: {letterGrade}{sign}");
+
+        if(gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You Passed!");
+        }
+        else 
+        {
+            Console.WriteLine("Stay focused and you'll get it next time!");
+        }
+
     }
 }
