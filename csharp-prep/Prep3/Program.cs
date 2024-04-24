efd94b0b2b3f48e1add39adfb28856836829444d
jgcnsny <thegreathappyvirus@gmail.com>
2024-04-24 23:21:12 +0800

    c# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        
+
+        int guess_count = 0;
+
+        string play_again = "yes";
+
+        while(play_again == "yes")
+        {
+            int magic_number = randomGenerator.Next(1, 100);
+            Console.Write("What is your guess? ");
+            int guess_number = int.Parse(Console.ReadLine());
+
+             guess_count = guess_count + 1;
+
+             while(guess_number != magic_number)
+             {
+                if(guess_number > magic_number)
+                {
+                    Console.WriteLine("Lower");
+                    Console.Write("What is your guess? ");
+                    guess_number = int.Parse(Console.ReadLine());
+                    guess_count = guess_count + 1;
+                }
+                else if (guess_number < magic_number)
+                {
+                    Console.WriteLine("Higher");
+                    Console.Write("What is your guess? ");
+                    guess_number = int.Parse(Console.ReadLine());
+                    guess_count = guess_count + 1;
+                }
+                else
+                {
+                    guess_count = guess_count + 1;
+                    Console.WriteLine("You guessed it!");
+                }
+
+             }
+
+                Console.WriteLine($"It took you {guess_count} guesses");
+
+                Console.Write("Do you want to play again? yes or no: ");
+                play_again = Console.ReadLine();
+
+        }
+
+        Console.WriteLine("Thank you for playing!");
     }
 }
