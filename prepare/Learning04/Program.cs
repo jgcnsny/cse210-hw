66d1ff13188bd1bdeee30452d58bfc95aec7e632
jgcnsny <thegreathappyvirus@gmail.com>
2024-05-14 20:56:12 +0800

    Learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Samuel Bennett","Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment math1 = new MathAssignment("Roberto Rodriguez","Fraction","Section 7.3", "Problems 8-19");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        WritingAssignment writing1 = new WritingAssignment("Mary Water","European History","The Causes of World War II");
+        Console.WriteLine(writing1.GetSummary());
+        Console.WriteLine(writing1.GetWrtingInformation());
     }
 }
