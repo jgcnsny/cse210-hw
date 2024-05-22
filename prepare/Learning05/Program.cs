2e686353fcfd32f10f4111dfa4e8d17d8c603c57
jgcnsny <thegreathappyvirus@gmail.com>
2024-05-22 20:14:50 +0800

    Learning 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+       List<Shape> shapes = new List<Shape>();
+
+       Square square = new Square("pink", 2);
+       //Console.WriteLine(square.GetColor());
+       //Console.WriteLine(square.GetArea());
+       shapes.Add(square);
+
+       Rectangle rectangle = new Rectangle("blue", 3, 2);
+       //Console.WriteLine(rectangle.GetColor());
+       //Console.WriteLine(rectangle.GetArea());
+       shapes.Add(rectangle);
+
+       Circle circle = new Circle("yellow", 5);
+       //Console.WriteLine(circle.GetColor());
+       //Console.WriteLine(circle.GetArea());
+       shapes.Add(circle);
+
+       foreach(Shape shape in shapes)
+       {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"Color: {color} Area: {area}");
+
+       }
+
     }
 }
