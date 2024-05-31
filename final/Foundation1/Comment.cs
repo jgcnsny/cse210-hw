033cee0c62aaa1ff219522364412c393b03236e1
jgcnsny <thegreathappyvirus@gmail.com>
2024-05-31 19:46:51 +0800

    Foundation 1

--- final/Foundation1/Comment.cs
+++ final/Foundation1/Comment.cs
@@ -2,11 +2,17 @@ using System;
 
 public class Comment
 {
-    public string _authorName;
+    public string _userName;
     public string _comment;
+
+    public Comment(string userName, string comment)
+    {
+        _userName = userName;
+        _comment = comment;
+    }
 
     public void DisplayComment()
     {
-        Console.WriteLine($"{_comment} - {_authorName}");
+        Console.WriteLine($"> {_userName}: {_comment}");
     }
 }
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.CompilerServices;
 using System.Transactions;
 
 public class Video 
@@ -8,17 +9,29 @@ public class Video
     public int _length;
 
     public List<Comment> _comments = new List<Comment>();
+
+    public Video(string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+    }
     
+    public void  StoreComment(Comment comment)
+    {
+        _comments.Add(comment);  
+    }
 
     public void DisplayVideoInfo()
     {
         Console.WriteLine($"{_title} - {_author} ");
-        Console.WriteLine($"{_length} seconds");
-    }
+        Console.WriteLine($"({_length} seconds)");
+        Console.WriteLine($"Comments: {CommentCount()}");   
 
-    public void  StoreComment(Comment comment)
-    {
-        _comments.Add(comment);  
+        foreach (var comment in _comments)
+            {
+                comment.DisplayComment();
+            }
     }
 
     public int CommentCount()
@@ -26,12 +39,5 @@ public class Video
         return _comments.Count;
     }
 
-    public void DisplayAllComments()
-        {
-            foreach (var comment in _comments)
-            {
-                comment.DisplayComment();
-            }
-        }
 
 }
